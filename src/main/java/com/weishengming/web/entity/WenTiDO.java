69cd78a2b7d1a7503d1ea64f1f474ba137e0958b
weishengming <15711018185@163.com>
2015-12-22 18:25:24 +0800

    提交

--- src/main/java/com/weishengming/web/entity/WenTiDO.java
+++ src/main/java/com/weishengming/web/entity/WenTiDO.java
@@ -12,15 +12,18 @@ public class WenTiDO {
 	private String tiwen_miaoshu; //描述
 	private String tijiaoshijian_string;//提交时间
 	private String zhaojichengxu; //问题的着急程度 [1不着急,2很着急,3特别着急,4马上向得到答案]
-	private String jiejuezhuangtai; //解决状态 [1 已解决 2.正在解决]
+	private String zhaojichengxu_string;
+	private String weitizhuangtai; //问题状态 [1 已解决 2.正在解决 3 已解决] 数字用来排序
+	private String weitizhuangtai_string;
 	
 	
 	
-	public String getJiejuezhuangtai() {
-		return jiejuezhuangtai;
+ 
+	public String getWeitizhuangtai() {
+		return weitizhuangtai;
 	}
-	public void setJiejuezhuangtai(String jiejuezhuangtai) {
-		this.jiejuezhuangtai = jiejuezhuangtai;
+	public void setWeitizhuangtai(String weitizhuangtai) {
+		this.weitizhuangtai = weitizhuangtai;
 	}
 	public String getId() {
 		return id;
