fe7892e4e49c5740ed307d6e2cbd7655c5f62b7e
weishengming <15711018185@163.com>
2016-01-28 15:24:20 +0800

    修改完成

--- src/main/java/com/weishengming/service/DiXiongZiMeiService.java
+++ src/main/java/com/weishengming/service/DiXiongZiMeiService.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Service;
 
 import com.weishengming.common.validate.ValidationService;
 import com.weishengming.dao.entity.DiXiongZiMeiDO;
+import com.weishengming.dao.entity.JiaoTangDO;
 import com.weishengming.dao.mapper.DiXiongZiMeiMapper;
 import com.weishengming.dao.param.DiXiongZiMeiParam;
+import com.weishengming.dao.param.JiaoTangParam;
 import com.weishengming.dao.query.DiXiongZiMeiQuery;
+import com.weishengming.dao.query.JiaoTangQuery;
 import com.weishengming.dao.query.ResultPage;
 
 @Service
@@ -28,7 +31,17 @@ public class DiXiongZiMeiService {
         BeanUtils.copyProperties(query, param);
         List<DiXiongZiMeiDO> list = mapper.findList(param);
 
-        return new ResultPage<DiXiongZiMeiDO>(list, query);
+        ResultPage<DiXiongZiMeiDO> result=new ResultPage<DiXiongZiMeiDO>(list, query);
+        Long count = this.countList(query);
+        int total = count.intValue();
+        int totalPage = total / result.getPageSize();
+        if (totalPage != 0 && total % result.getPageSize() != 0) {
+            totalPage += 1;
+        }
+        result.setCount(count); //设置总条数
+        result.setTotal(totalPage); //谁知总页数
+        
+        return result;
     }
 
     public List<DiXiongZiMeiDO> findAll() {
@@ -50,12 +63,18 @@ public class DiXiongZiMeiService {
         mergeEntity(entity,oldEntity);
         mapper.update(oldEntity);
     }
+    
+    public Long countList(DiXiongZiMeiQuery query){
+    	DiXiongZiMeiParam param = new DiXiongZiMeiParam();
+        BeanUtils.copyProperties(query, param);
+        return mapper.countList(param);
+     }
 
     public void delete(Long id) {
         mapper.delete(id);
     }
     
     private void mergeEntity(DiXiongZiMeiDO source,DiXiongZiMeiDO target){
-    	//TODO 
+    	 
     }
 }
