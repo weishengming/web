73724bc249c295dda1c477d418494fcf0ff56ebc
weishengming <15711018185@163.com>
2016-02-16 09:27:13 +0800

    修改

--- src/main/java/com/weishengming/dao/query/DiXiongZiMeiQuery.java
+++ src/main/java/com/weishengming/dao/query/DiXiongZiMeiQuery.java
@@ -4,4 +4,19 @@ package com.weishengming.dao.query;
 
 public class DiXiongZiMeiQuery extends MapperQuery{
     private static final long serialVersionUID = 0L;
+    private String xingming;
+    private String shoujihao;
+	public String getXingming() {
+		return xingming;
+	}
+	public void setXingming(String xingming) {
+		this.xingming = xingming;
+	}
+	public String getShoujihao() {
+		return shoujihao;
+	}
+	public void setShoujihao(String shoujihao) {
+		this.shoujihao = shoujihao;
+	}
+    
 }
