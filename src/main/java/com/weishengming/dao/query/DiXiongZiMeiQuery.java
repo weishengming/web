4a453d65b49340f2cdba7110e872e9aaf382f97d
yangtianci <yangtianci@99fenqi.com>
2016-05-04 16:27:07 +0800

    修改

--- src/main/java/com/weishengming/dao/query/DiXiongZiMeiQuery.java
+++ src/main/java/com/weishengming/dao/query/DiXiongZiMeiQuery.java
@@ -1,46 +1,60 @@
 package com.weishengming.dao.query;
 
-
-
-public class DiXiongZiMeiQuery extends MapperQuery{
+public class DiXiongZiMeiQuery extends MapperQuery {
     private static final long serialVersionUID = 0L;
-    private String xingming;
-    private String shoujihao;
-    private String shenfen;
-    private String suoding;
-    private String xingbie;
-    
-    
-    
-	public String getXingbie() {
-		return xingbie;
-	}
-	public void setXingbie(String xingbie) {
-		this.xingbie = xingbie;
-	}
-	public String getShenfen() {
-		return shenfen;
-	}
-	public void setShenfen(String shenfen) {
-		this.shenfen = shenfen;
-	}
-	public String getSuoding() {
-		return suoding;
-	}
-	public void setSuoding(String suoding) {
-		this.suoding = suoding;
-	}
-	public String getXingming() {
-		return xingming;
-	}
-	public void setXingming(String xingming) {
-		this.xingming = xingming;
-	}
-	public String getShoujihao() {
-		return shoujihao;
-	}
-	public void setShoujihao(String shoujihao) {
-		this.shoujihao = shoujihao;
-	}
-    
+    private String            xingming;
+    private String            shoujihao;
+    private String            shenfen;
+    private String            suoding;
+    private String            xingbie;
+    private String            nianling;
+
+    public String getNianling() {
+        return nianling;
+    }
+
+    public void setNianling(String nianling) {
+        this.nianling = nianling;
+    }
+
+    public String getXingbie() {
+        return xingbie;
+    }
+
+    public void setXingbie(String xingbie) {
+        this.xingbie = xingbie;
+    }
+
+    public String getShenfen() {
+        return shenfen;
+    }
+
+    public void setShenfen(String shenfen) {
+        this.shenfen = shenfen;
+    }
+
+    public String getSuoding() {
+        return suoding;
+    }
+
+    public void setSuoding(String suoding) {
+        this.suoding = suoding;
+    }
+
+    public String getXingming() {
+        return xingming;
+    }
+
+    public void setXingming(String xingming) {
+        this.xingming = xingming;
+    }
+
+    public String getShoujihao() {
+        return shoujihao;
+    }
+
+    public void setShoujihao(String shoujihao) {
+        this.shoujihao = shoujihao;
+    }
+
 }
